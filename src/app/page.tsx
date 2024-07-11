c9d741f886ea87e6cadcbbe00804c5ba745891ea
Aphidet-Hongseetong <ottooffple@icloud.com>
2024-07-11 13:02:16 +0700

    feat: Add multilingual support to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,14 @@ import { Navbar } from '@/components/Navbar'
 
 // Export default function
 export default function Home() {
-  return <Navbar />
+  return (
+    <>
+      <Navbar />
+      <div className='p-10'>
+        <div className='mockup-window bg-base-300 border'>
+          <div className='bg-base-200 flex justify-center px-4 py-16'>สวัสดีชาวโลก นี้คือ NextJS v.15</div>
+        </div>
+      </div>
+    </>
+  )
 }
